f2af3ef041c18eff8f2fc41aab14ca7ed4ef981b
coolestProcaviidae <85066178+genericWindowsUser416@users.noreply.github.com>
2024-12-01 18:03:00 +0300

    updated UserQueries.cs

--- PenkiControlApp.Core/Constants.cs
+++ PenkiControlApp.Core/Constants.cs
@@ -5,11 +5,7 @@ public static class Constants
     public const string CONNECTION_INFO = "Server=localhost;Port=5432;User Id=postgres;Password=azure;Database=Testik";
 
 
-    public const string INSERT_USER_QUERY =
-        "insert into public.\"User\" (\"Name\", \"Login\", \"Password\") values (@Name, @Login, @Password) returning \"Id\"";
 
-    public const string GET_USER_BY_ID_QUERY =
-        "select \"Id\", \"Name\", \"Login\", \"Password\" from \"User\" where \"Id\"=@Id";
 
     public const string DO_QQQ = "select * from \"Order\"";
 }
--- PenkiControlApp.Core/Queries/UserQueries.cs
+++ PenkiControlApp.Core/Queries/UserQueries.cs
@@ -3,5 +3,10 @@
     public class UserQueries
     {
         public const string GET_USERS_QUERY = "SELECT * FROM \"User\"";
+        public const string INSERT_USER_QUERY =
+    "insert into public.\"User\" (\"Name\", \"Login\", \"Password\") values (@Name, @Login, @Password) returning \"Id\"";
+
+        public const string GET_USER_BY_ID_QUERY =
+            "select \"Id\", \"Name\", \"Login\", \"Password\" from \"User\" where \"Id\"=@Id";
     }
 }
