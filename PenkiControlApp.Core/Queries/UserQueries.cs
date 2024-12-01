b4584c0fa997afa96746df356ee036965c93b6b1
coolestProcaviidae <85066178+genericWindowsUser416@users.noreply.github.com>
2024-12-01 18:09:38 +0300

    Update UserQueries.cs

--- PenkiControlApp.Core/Queries/UserQueries.cs
+++ PenkiControlApp.Core/Queries/UserQueries.cs
@@ -7,6 +7,14 @@
     "insert into public.\"User\" (\"Name\", \"Login\", \"Password\") values (@Name, @Login, @Password) returning \"Id\"";
         public const string GET_USER_BY_ID_QUERY =
             "select \"Id\", \"Name\", \"Login\", \"Password\" from \"User\" where \"Id\"=@Id";
+<<<<<<< HEAD
 
+=======
+<<<<<<< Updated upstream
+=======
+>>>>>>> main
+=======
+>>>>>>> Stashed changes
+>>>>>>> 943e985 (Update UserQueries.cs)
     }
 }
