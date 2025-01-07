8c074e4abb1e94f45c4707371c9341d246701aad
coolestProcaviidae <85066178+genericWindowsUser416@users.noreply.github.com>
2025-01-07 17:59:47 +0300

    created CategoryWindowTag.xaml

---
+++ PenkiControlApp.UI/Windows/CategoryWindowTag.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace PenkiControlApp.UI.Windows;
+
+public partial class CategoryWindowTag : UserControl
+{
+    public CategoryWindowTag()
+    {
+        InitializeComponent();
+    }
+}
